0b0588ab9bcfb27e6cdd0d1106a8c4e64e85edfa
Gabe Hargrave <gkhargrave@gmail.com>
2023-06-04 19:21:51 -0700

    Move all data-fetching client-side to fix caching issues

--- src/app/bookmarks/Bookmarks.container.tsx
+++ src/app/bookmarks/Bookmarks.container.tsx
@@ -1,32 +1,50 @@
 "use client";
+
 import React from "react";
 
+import { useAsync, useMountEffect } from "@react-hookz/web";
+
+import { fetchAll } from "@/lib/api/api";
+import { Spinner } from "@/lib/components";
 import type { BkmGroup, Bookmark } from "./bookmarks.types";
 import { BkmControlBar, BkmHeader, BkmList } from "./_components";
 import { useBkmGroupsStore, useBookmarksStore } from "./_store";
 
-interface Props {
-  bkmGroups: BkmGroup[];
-  bookmarks: Bookmark[];
-}
-
-export default function BookmarksContainer({ bkmGroups, bookmarks }: Props) {
+// TODO: add a version number display
+export default function BookmarksContainer() {
   const setBookmarks = useBookmarksStore((state) => state.setData);
   const setBkmGroups = useBkmGroupsStore((state) => state.setData);
 
-  React.useEffect(() => {
-    setBkmGroups(bkmGroups);
-  }, [bkmGroups, setBkmGroups]);
+  const [dataFetchState, dataFetchFn] = useAsync(async () =>
+    Promise.all([
+      fetchAll<Bookmark>("bookmarks"),
+      fetchAll<BkmGroup>("bkmGroups"),
+    ])
+  );
 
   React.useEffect(() => {
-    setBookmarks(bookmarks);
-  }, [bookmarks, setBookmarks]);
+    if (Array.isArray(dataFetchState.result)) {
+      const [bookmarks = [], bkmGroups = []] = dataFetchState.result;
+      setBookmarks(bookmarks);
+      setBkmGroups(bkmGroups);
+    }
+  }, [dataFetchState.result, setBkmGroups, setBookmarks]);
+
+  useMountEffect(() => {
+    dataFetchFn.execute();
+  });
+
+  const isLoading =
+    dataFetchState.status === "not-executed" ||
+    dataFetchState.status === "loading";
 
   return (
     <>
       <BkmHeader />
       <BkmControlBar />
       <BkmList />
+      {/* TODO: make a better loader */}
+      {isLoading && <Spinner />}
     </>
   );
 }
--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,32 +1,11 @@
-import type { BkmGroup, Bookmark } from "@/app/bookmarks/bookmarks.types";
-import { fetchAll } from "@/lib/api/api";
 import BookmarksContainer from "./Bookmarks.container";
 
 // TODO: figure out how to reduce initial size
 
-export const revalidate = 1;
-
-async function fetchData(): Promise<{
-  bkmGroups: BkmGroup[];
-  bookmarks: Bookmark[];
-}> {
-  const [bookmarks, bkmGroups] = await Promise.all([
-    fetchAll<Bookmark>("bookmarks"),
-    fetchAll<BkmGroup>("bkmGroups"),
-  ]);
-
-  return {
-    bookmarks,
-    bkmGroups,
-  };
-}
-
 export default async function BookmarksPage() {
-  const { bkmGroups, bookmarks } = await fetchData();
-
   return (
     <main>
-      <BookmarksContainer bkmGroups={bkmGroups} bookmarks={bookmarks} />
+      <BookmarksContainer />
     </main>
   );
 }
